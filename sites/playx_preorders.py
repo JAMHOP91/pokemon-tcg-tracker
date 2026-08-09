56fe9296fd9d509aa329059d07fcd25ba5450d48
James Hopwood <jameshopwoodnz@gmail.com>
2026-08-09 12:17:36 +1200

    PlayX: also check pokemon-2 collection for the announced 30th Celebration pre-order event

--- sites/playx_preorders.py
+++ sites/playx_preorders.py
@@ -1,14 +1,26 @@
 ﻿"""
-PlayX - Pokemon Pre-Orders. Uses the shared Shopify helper.
+PlayX - Pokemon Pre-Orders. Uses the shared Shopify helper. Checks
+BOTH their general pre-order collection AND the specific
+"pokemon-2" collection used for the announced 30th Celebration
+pre-order event (Aug 9, 2026).
 """
 
 from sites.shopify_helper import get_shopify_products
 
 SITE_NAME = "PlayX - Pokemon Pre-Orders"
 BASE_URL = "https://www.playx.co.nz"
-COLLECTION_HANDLE = "pre-order"
 ALLOW_EMPTY_RESULTS = True
 
 
 def get_current_products() -> list[dict]:
-    return get_shopify_products(BASE_URL, collection_handle=COLLECTION_HANDLE, require_keywords=["pokemon"])
+    all_products = {}
+
+    general = get_shopify_products(BASE_URL, collection_handle="pre-order", require_keywords=["pokemon"])
+    for p in general:
+        all_products[p["id"]] = p
+
+    celebration = get_shopify_products(BASE_URL, collection_handle="pokemon-2")
+    for p in celebration:
+        all_products[p["id"]] = p
+
+    return list(all_products.values())
